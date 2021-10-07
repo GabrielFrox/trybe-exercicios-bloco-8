7a4e7fab587db27dd86c5a0ecd16de2485fac05c
Gabriel Oliveira <gabrielfrox@gmail.com>
2021-10-07 19:59:20 -0300

    Exercicio 4 OK

--- dia-4-javascript-es6-higher-order-functions-reduce/exercicio4.js
+++ dia-4-javascript-es6-higher-order-functions-reduce/exercicio4.js
@@ -62,3 +62,21 @@ const books = [
 ];
 
 // Adicione o código do exercício aqui:
+
+const expectedResult = {
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  genre: 'Fantasia',
+  author: {
+    name: 'George R. R. Martin',
+    birthYear: 1948,
+  },
+  releaseYear: 1991,
+};
+
+function longestNamedBook() {
+  // escreva seu código aqui
+  return books.reduce((biggestName, currentName) => (biggestName.name.length < currentName.name.length) ? biggestName = currentName : biggestName)
+}
+
+console.log(longestNamedBook());
